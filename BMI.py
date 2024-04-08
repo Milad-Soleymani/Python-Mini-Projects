69908018d6d773e8a196b7ab91e9acb94cf9509b
MiladCoder <miladcoder1390@gmail.com>
2024-04-08 16:56:16 +0330

    update BMI file

--- BMI.py
+++ BMI.py
@@ -1,4 +1,9 @@
 import turtle as Tur
 
-height = int(input('What is your height?'))
-mass = int(input('What is your crime?'))
+height = int(input('What is your height? (by cm)'))
+mass = int(input('What is your crime? (by kg)'))
+
+height = height / 100
+
+BMI = mass / (height ** 2)
+
