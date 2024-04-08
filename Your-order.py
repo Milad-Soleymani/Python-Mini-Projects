96a37762c09c9a029d5264045c45c0af7e7005e6
MiladCoder <miladcoder1390@gmail.com>
2024-04-08 16:02:50 +0330

    add rotate degree input

--- Your-order.py
+++ Your-order.py
@@ -3,6 +3,7 @@ import turtle as Tur
 side = Tur.textinput("Create shape But with your says ", "be polygonal?")
 width = Tur.textinput("Create shape But with your says ", "What is the length of the shape?")
 repeat = Tur.textinput("Create shape But with your says ", "How many times to repeat?")
+rotate = Tur.textinput("Create shape But with your says ", "How many degrees does it rotate in each round?")
 
 
 
