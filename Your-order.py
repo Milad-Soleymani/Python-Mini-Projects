44210c0a0a655d4a1f8390e4a54c4a52fdca7bfe
MiladCoder <miladcoder1390@gmail.com>
2024-04-08 16:01:34 +0330

    add repeat times input

--- Your-order.py
+++ Your-order.py
@@ -2,6 +2,7 @@ import turtle as Tur
 
 side = Tur.textinput("Create shape But with your says ", "be polygonal?")
 width = Tur.textinput("Create shape But with your says ", "What is the length of the shape?")
+repeat = Tur.textinput("Create shape But with your says ", "How many times to repeat?")
 
 
 
