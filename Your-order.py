fbdc17f2547ff8780bbde50fd5c80a76cf939a2b
MiladCoder <miladcoder1390@gmail.com>
2024-04-09 14:27:08 +0330

    update and add color option

--- Your-order.py
+++ Your-order.py
@@ -1,18 +1,18 @@
 import turtle as Tur
 
 print("if Do not answer the questions, You will get an error ")
-side =  int(input( "be polygonal?"))
-width =  int(input( "What is the length of the shape?"))
-repeat =  int(input( "How many times to repeat?"))
-rotate = int(input( "How many degrees does it rotate in each round?"))
+side =  int(input( "be polygonal? "))
+width =  int(input( "What is the length of the shape? "))
+repeat =  int(input( "How many times to repeat? "))
+rotate = int(input( "How many degrees does it rotate in each round? "))
+color =  input("Do you want wich once color your shape ? ")
 
 Tur.speed('fastest')
 
 for j in range(repeat):
+    Tur.color(color)
     Tur.width(2)
     for i in range(side):
         Tur.forward(width)
         Tur.right(360 / side)
     Tur.right(rotate)
-
-Tur.done()
