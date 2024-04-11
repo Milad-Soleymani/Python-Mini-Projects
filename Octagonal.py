f4fb1134fbf945239bd6bb7088451d2029561487
MiladCoder <miladcoder1390@gmail.com>
2024-04-11 13:37:06 +0330

    add comment to peoject

--- Octagonal.py
+++ Octagonal.py
@@ -1,15 +1,17 @@
 import turtle as Tur
 
-Tur.speed('fastest')
-Tur.width(2)
+Tur.speed('fastest') # change speed to fastest mode
+
+Tur.width(2) # change thickness of pen
+
 for j in range(12):
-    for i in range(8):
-        Tur.forward(70)
-        Tur.right(45)
-    Tur.rt(30)
-Tur.color('red')
+    for i in range(8):   # insert ocatagonal
+        Tur.forward(70)  # shape side length
+        Tur.right(45)    
+    Tur.rt(30) # set rotate degree in every repeat 
+Tur.color('red') # change color to red
 Tur.penup()
-Tur.goto(-300,-300)
+Tur.goto(-300,-300) # Change Location of oen
 Tur.pendown()
-Tur.write('Milad Soleymani')
-Tur.done()
+Tur.write('Milad Soleymani') # write namme
+Tur.done() # for don't closing after completed codes
