427d2b1e04f349939380671e9e4aeb17102dbbea
MiladCoder <miladcoder1390@gmail.com>
2024-05-07 22:29:01 +0330

    Update repo

---
+++ learn.py
@@ -0,0 +1,14 @@
+import sys
+print(sys.version)
+
+# this is a inline comment
+
+"""
+this
+is
+a
+multiline
+comment
+"""
+
+print('Hi')
--- turtle/Your-order.py
+++ turtle/Your-order.py
@@ -1,10 +1,11 @@
-import turtle as Tur
+import turtle 
+
+Tur = turtle.Turtle()
 
 print("if Do not answer the questions, You will get an error ")
 side =  int(input( "be polygonal? "))
 width =  int(input( "What is the length of the shape? "))
 repeat =  int(input( "How many times to repeat? "))
-rotate = int(input( "How many degrees does it rotate in each round? "))
 color =  input("Do you want wich once color your shape ? ")
 
 Tur.speed('fastest')
@@ -15,4 +16,5 @@ for j in range(repeat):
     for i in range(side):
         Tur.forward(width)
         Tur.right(360 / side)
-    Tur.right(rotate)
+    Tur.right(360 / repeat)
+turtle.done
